90353ea262c97fb15572d7346a3b869d022e2ef9
pkamibayashi <paulo_kamibayashi@hotmail.com>
2023-01-20 21:15:11 -0300

    corrected removeProduct function bug

--- src/index.js
+++ src/index.js
@@ -25,24 +25,41 @@ function calculateAll() {
 function removeProduct(event) {
   let target = event.currentTarget;
   console.log(target);
-  let parent = target.parentNode.parentNode;
-  let subTotal = parent.childNodes[7];
-  let subTotalPrice = subTotal.innerText;
-  parent.parentNode.removeChild(parent);
-  calculateAll() = subTotalPrice -= calculateAll;
+  let fatherNode = target.parentNode.parentNode;
+
+  let priceElement = fatherNode.querySelector(".subtotal span");
+  let totalElement = document.getElementById("Subtotal");
+  console.log(totalElement);
+  let priceNumber = Number(priceElement.innerText);
+  let totalNumber = Number(totalElement.innerText);
+  totalElement.innerText = totalNumber -= priceNumber;
+  fatherNode.parentNode.removeChild(fatherNode);
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  // let productNameField = document.getElementById('productName').value
+  // const setPrice = document.getElementById('setPrice').value
+  // const productsRoll = document.querySelector('.product')
+  // const clone = productsRoll.cloneNode(true)
+  // console.log(clone)
+  // const newProductPrice = document.querySelector('.price span')
+  // const newProductName = document.querySelector('.name span')
+  // newProductName.innerText = productNameField
+  // newProductPrice.innerText= setPrice
+  // const tbody = document.getElementById('produtos')
+  // tbody.insertAdjacentElement("beforeend", clone)
 }
 
 window.addEventListener("load", () => {
+  createProduct();
   const calculatePricesBtn = document.getElementById("calculate");
   calculatePricesBtn.addEventListener("click", calculateAll);
   const removeBtn = document.querySelectorAll(".btn-remove ");
+  const createBtn = document.getElementById("create");
   console.log(removeBtn);
+  createBtn.addEventListener("click", createProduct);
   for (let i = 0; i < removeBtn.length; i++) {
     removeBtn[i].addEventListener("click", removeProduct);
   }
